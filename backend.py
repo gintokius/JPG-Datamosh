a4ee249ac39728aa797e51f9a6abbb3016f0d565
b1tpixel <gintokius@gmail.com>
2017-06-03 02:33:01 +0300

    Convert to python 3.6
    Fuck python 3.6

--- backend.py
+++ backend.py
@@ -7,20 +7,20 @@ def findDataEntry(data):
 def rewriteCodeSequence(data, pos):
 	length = pos + random.randint(1,15)
 	if length < len(data):
-		for i in xrange(pos, length):
-			data[i] = hex(random.randint(10,100))
+		for i in range(pos, length):
+			data[i] = random.randint(1,255)
 	return data
 
 def deleteByte(data):
 	startPos = random.randint(0,len(data))
 	amount = random.randint(1,5)
-	for i in xrange(startPos,startPos+amount):
+	for i in range(startPos,startPos+amount):
 		if startPos + amount < len(data):
 			del data[i]
 	return data
 
 def corruptData(data,intensity):
-	for i in xrange(1,intensity):
+	for i in range(1,intensity):
 		if random.randint(1,100) > 80:
 			pos = random.randint(0,len(data))
 			data = rewriteCodeSequence(data, pos)
@@ -29,22 +29,19 @@ def corruptData(data,intensity):
 	return data
 
 
-
-
 def main():
 	with open('picture.jpg','rb') as fd:
-		jpgdata = fd.read()
+		jpgdata = bytearray(fd.read())
 
 	if jpgdata.startswith(b'\xFF\xD8'):
 		dataStart = findDataEntry(jpgdata)
-		heading = list(jpgdata[:dataStart])
-		data = list(jpgdata[dataStart:-10])
-		ending = list(jpgdata[-10:])
+		heading = jpgdata[:dataStart]
+		data = jpgdata[dataStart:-10]
+		ending = jpgdata[-10:]
 		corruptData(data, 10)
 		jpgdata = heading + data + ending
 		with open('result.jpg','wb') as fd:
-			for i in xrange(0,len(jpgdata)):
-				fd.write(jpgdata[i])
+			fd.write(jpgdata)
 			print('success')
 
 if __name__ == '__main__':
